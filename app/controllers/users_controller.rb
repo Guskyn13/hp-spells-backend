8683330713dd24349d1f1e8c41546374ee3ebd2c
Kristine Du <kristine.a.du@gmail.com>
2020-09-16 16:30:25 -0400

    Update show render.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
 
-        render json: @user, include: spells
+        render json: @user, include: :spells
     end
 
     def create
